2fa3ec85166892e6327f7e768b7ba39328ec3718
mstafa-krd <mstafamzere1@gmail.com>
2024-09-20 23:16:59 +0300

    bug fix

--- src/config/CheckAuth.jsx
+++ src/config/CheckAuth.jsx
@@ -8,17 +8,20 @@ import { authApi } from "../api/authApi.jsx";
 const CheckAuth = () => {
   const [auth, setAuth] = useState();
   const [loader, setLoader] = useState(true);
-  const { user,setUser } = useContext(UserContext);
+  const { user, setUser } = useContext(UserContext);
 
   useEffect(() => {
     axios
       .get(authApi, { withCredentials: true })
       .then((res) => {
-        console.log(res.data.user)
+        console.log(res.data.user);
         setAuth(res.data);
         setUser(res.data.user);
         localStorage.setItem("user", true);
         setLoader(false);
+        if (user) {
+          setAuth({ authenticated: true });
+        }
       })
       .catch((err) => {
         console.error("Error checking authentication status", err);
