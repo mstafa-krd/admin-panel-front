d366dbfadaf6599fa7cca6d260eb1248085fdf9e
mstafa-krd <mstafamzere1@gmail.com>
2024-09-21 15:38:08 +0300

    bug fix

--- src/config/CheckAuth.jsx
+++ src/config/CheckAuth.jsx
@@ -10,24 +10,23 @@ const CheckAuth = () => {
   const [loader, setLoader] = useState(true);
   const { user, setUser } = useContext(UserContext);
 
-  useEffect(() => {
-    axios
-      .get(authApi, { withCredentials: true })
-      .then((res) => {
-        console.log(res.data.user);
-        setAuth(res.data);
-        setUser(res.data.user);
-        localStorage.setItem("user", true);
-        setLoader(false);
-        if (user) {
-          setAuth({ authenticated: true });
-        }
-      })
-      .catch((err) => {
-        console.error("Error checking authentication status", err);
-        setLoader(false);
-      });
-  }, [setUser]);
+ useEffect(() => {
+   axios
+     .get(authApi, { withCredentials: true })
+     .then((res) => {
+       console.log("User Data:", res.data.user);
+       console.log("Auth Status:", res.data.authenticated);
+       setAuth(res.data);
+       setUser(res.data.user);
+     })
+     .catch((err) => {
+       console.error("Error checking authentication status", err);
+       setAuth({ authenticated: false });
+     })
+     .finally(() => {
+       setLoader(false);
+     });
+ }, [setUser]);
 
   return loader ? (
     <Loader />
